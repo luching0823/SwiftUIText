a17cf65a6df6950dfd37bdd85a84d46d631ce649
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 11:04:26 +0800

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by 廖昱晴 on 2021/3/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
